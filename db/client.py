06b3514d54a8be9bc6ca8855072d30d9011b2416
Amber Wang <aw896@cornell.edu>
2026-06-26 18:23:34 -0400

    clean code for docker run

--- db/client.py
+++ db/client.py
@@ -1,7 +1,5 @@
 # Establish a connection to the database
 import os
-from dotenv import load_dotenv
 from supabase import create_client
-import pathlib 
-load_dotenv(pathlib.Path(__file__).parent.parent / '.env')
+
 supabase = create_client(os.getenv("DB_URL"), os.getenv("DB_SERVICE_KEY"))
